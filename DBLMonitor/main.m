d33b8bb393274e8436a5f379d2dd2bf7f4840414
Bob Dale <bob.dale2333@gmail.com>
2019-07-30 22:54:38 +0800

    init

---
+++ DBLMonitor/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  DBLMonitor
+//
+//  Created by LeeeBaoy on 2019/7/30.
+//  Copyright © 2019 Bob.Dale. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
